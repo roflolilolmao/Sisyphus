831375b3ac386700ba08be010dd86fe28aa7d447
lfatton <louise.fatton@hotmail.fr>
2019-07-06 17:28:31 +0200

    merged

--- js/character.js
+++ js/character.js
@@ -1,25 +1,59 @@
-const CHARACTER_WIDTH = 20
-const CHARACTER_HEIGHT = 80
-
 class Character
 {
     constructor(x_position, scene)
     {
         this.height = scene.ground_height_at(x_position)
         this.x_position = x_position
-        
-        this.graphics = new PIXI.Graphics()
-        app.stage.addChild(this.graphics)
+
+        this.container = new PIXI.Container()
+        app.stage.addChild(this.container)
     }
 
     drow()
     {
-        this.graphics.position.set(0, 0)
-        this.graphics.beginFill(0xFF00000)
-        this.graphics.drawRect(0, 0, CHARACTER_WIDTH, CHARACTER_HEIGHT)
-        this.graphics.endFill()
-        this.graphics.position.set(
-            this.x_position,
-            this.height - this.graphics.height)
+        let left_leg = new PIXI.Graphics();
+        this.container.addChild(left_leg);
+
+        left_leg.position.set(0, 40);
+        left_leg.beginFill(0xFF0000);
+        left_leg.drawRect(0, 0, 5, 25);
+        left_leg.endFill();
+        left_leg.rotation = 0.5;
+
+        let right_leg = new PIXI.Graphics();
+        this.container.addChild(right_leg);
+
+        right_leg.position.set(0, 40);
+        right_leg.beginFill(0xFFFF00);
+        right_leg.drawRect(0, 0, 5, 25);
+        right_leg.endFill();
+        right_leg.rotation = -0.5;
+
+        let body = new PIXI.Graphics();
+        this.container.addChild(body);
+
+        body.position.set(0, 5);
+        body.beginFill(0x00FF00);
+        body.drawRect(0, 0, 5, 35);
+        body.endFill();
+
+        let arms = new PIXI.Graphics();
+        this.container.addChild(arms);
+
+        arms.position.set(0, 15);
+        arms.beginFill(0x0000FF);
+        arms.drawRect(0, 0, 5, 20);
+        arms.endFill();
+        arms.rotation = -1.8;
+
+        let head = new PIXI.Graphics();
+        this.container.addChild(head);
+
+        head.position.set(0, 0);
+        head.beginFill(0x0FFFF0);
+        head.drawCircle(0, 0, 8);
+        head.endFill();
+
+        this.container.position.set(60, this.height - this.container.height + 10);
     }
 }
