73a57eb9522698865f7de077435fa2fc5bc98166
roflolilolmao <qjeanmon@gmail.com>
2019-07-08 19:38:15 +0200

    text

--- js/hud.js
+++ js/hud.js
@@ -17,7 +17,7 @@ class Hud
         this.hud.removeChild(this.text_gold)
 
         this.meter = -Math.round(scene.ground.height_at(scene.character.x_position) / GROUND_SEGMENTS_LENGTH)
-        this.text_meters = new PIXI.Text("Meters: " + this.meter, {fontSize: 40, fill: 0xbd3f1c, fontWeight: 600})
+        this.text_meters = new PIXI.Text("Altitude: " + this.meter, {fontSize: 40, fill: 0xbd3f1c, fontWeight: 600})
 
         this.text_bpm = new PIXI.Text("BPM: " + parseFloat(current_bpm).toFixed(1), {fontSize: 40, fill: 0xbd3f1c, fontWeight: 600})
         this.text_bpm.position.y = this.text_meters.height + 2
